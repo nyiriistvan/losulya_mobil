9b68177a9826a3fc73ec493d9e2908b31f7ca50e
nyiriistvan <92971467+nyiriistvan@users.noreply.github.com>
2023-04-17 16:52:57 +0200

    Függőségek hozzáadása és a számítások megírása

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import { moderateScale } from 'react-native-size-matters';
 
-export default function App() {
+const App = () => {
+  const [chestCircumference, setChestCircumference] = useState('');
+  const [givenLength, setGivenLength] = useState('');
+  const [horseWeight, setHorseWeight] = useState(null);
+
+  const calculateHorseWeight = () => {
+    const chest = parseFloat(chestCircumference);
+    const length = parseFloat(givenLength);
+    const weight = (chest ** 2 * length) / 330;
+    setHorseWeight(weight.toFixed(2));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Input
+        label="Ló kerülete (CM)"
+        value={chestCircumference}
+        onChangeText={setChestCircumference}
+        keyboardType="numeric"
+      />
+      <Input
+        label="Ló hosszúsága (CM)"
+        value={givenLength}
+        onChangeText={setGivenLength}
+        keyboardType="numeric"
+      />
+      <Button
+        title="Ló súlyának számítása"
+        onPress={calculateHorseWeight}
+        disabled={!chestCircumference || !givenLength}
+      />
+      {horseWeight && (
+        <Text style={styles.result}>
+          Estimated Weight: {horseWeight} lbs
+        </Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
